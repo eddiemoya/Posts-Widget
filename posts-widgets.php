808acee15869bb504c8b502cf9dadb6d45f52faf
Eddie Moya <eddie.moya@gmail.com>
2012-07-26 17:32:25 -0500

    adding subwidget Content List

---
+++ posts-widgets.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Posts Widget Plugin
+Description: Starting point for building widgets quickly and easier
+Version: 0.6
+Author: Eddie Moya
+Author URI: http://eddiemoya.com/
+ */
+
+include (plugin_dir_path(__FILE__) . 'widgets/post-widgets.php');
+include (plugin_dir_path(__FILE__) . 'widgets/content-list.php');
+ ?>
